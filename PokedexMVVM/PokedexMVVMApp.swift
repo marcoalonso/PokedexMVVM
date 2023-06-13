d373652faab46bbc61981fe085f33e30bc20ef7b
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-13 09:15:48 -0600

    Getting Started

---
+++ PokedexMVVM/PokedexMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexMVVMApp.swift
+//  PokedexMVVM
+//
+//  Created by Marco Alonso Rodriguez on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
