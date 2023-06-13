eeed4d7a4a558818f3b76a7a3afddcb2f914201e
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-13 09:23:43 -0600

    Webservice added

---
+++ PokedexMVVM/PokemonModel.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonModel.swift
+//  PokedexMVVM
+//
+//  Created by Marco Alonso Rodriguez on 13/06/23.
+//
+
+import Foundation
+
+struct PokemonModel: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
